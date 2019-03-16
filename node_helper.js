161e176e4ece9e7b56e7c477c69cbb869946ad6c
jikagq <theo-nec@hotmail.fr>
2019-03-16 19:13:05 +0100

    nouvelle branche dev, debut de la v2

--- node_helper.js
+++ node_helper.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Magic Mirror
- * Module: Watchor
+ * Module: MMM-SR04
  * 
  * By Jikagq
  * MIT Licensed
@@ -14,17 +14,15 @@ const Gpio = require('onoff').Gpio;//on off gpio
 var moment = require('moment');
 
 var  presence=true;
-var hard=true;
+
 const exec = require('child_process').exec;
-var firstoff=false;
-var firston=false;
-var inhib=false;
+
+
+//moment("2010-10-20 4:30",       "YYYY-MM-DD HH:mm");   // parsed as 4:30 local time
+
 
 
 module.exports = NodeHelper.create({
-	
-
-	
 	
 	start: function () {
 		const self = this;
@@ -35,16 +33,16 @@ module.exports = NodeHelper.create({
 				self.initSensor();
             }
 		});	
-		
 		console.log('Starting Module: ' + this.name);	
 	},
 	
 	socketNotificationReceived: function (notification, payload) {
 		var sensor;
 		const self = this;
-		//this.buff = payload;
-		//this.config=payload;
         var distances;// variable distance créer
+		
+		var deepsleepst = moment(self.config.deepsleepstart,"HH:mm");   
+		var deepsleepnd = moment(self.config.deepsleepend,"HH:mm");
 
 		if (notification === 'WATCH') {
 			
@@ -69,65 +67,30 @@ module.exports = NodeHelper.create({
             var distest;
             distest=self.sensor();
             console.log(distest);
-           	//console.log('aaaaaaaaaaaaaaaaaaaaaaaa'+self.distances); 
- 
 			(function measure() {
 				//console.log('Bite my shiny metal ass '+self.distances);
 				if (!self.distances || self.distances.length === self.config.rate) {
-                //console.log('bbbbbbbbbbbbbbbbbbbb');  
-                if (self.distances) 
-                {
-                    //console.log('ccccccccccccccccccccccccccccccccc');        
-                    //print(distances);
-                    // ajouter un hystéresis
-                    var testpresence = statistics.median(self.distances);
-                    if(testpresence<=self.config.cstdist)
-                    {
-                        presence=true;
-                        console.log(presence);
-                        self.config.delay=1000;//10s
-                        if(firston==false)
-                        {
-                            firston=true;
-                            firstoff=false;
-							console.log('ecran on');
-                            //exec("/opt/vc/bin/tvservice -s").stdout.on('data', function(data) {
-							//if (data.indexOf("0x120002") !== -1)
-							//exec("/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7", null);
-							//});
-							//exec("/opt/vc/bin/tvservice --preferred && sudo chvt 6 && sudo chvt 7", null);//ajouter une ligne pour controller les sortie des gpio pour le relai
-							
-							if(hard==false)//exeption lorsqye que l'on revient avant l'heure théorique
-							{
-								console.log('reveil de l ecran + allumage hdmi');
-								console.log('hard on node helper');
-								setTimeout(function(){ self.relay.writeSync(0);}, 1000);
-		  	                    self.relay.writeSync(1); 
-								hard=true
-								self.sendSocketNotification("INHIB");
-							}
+					if (self.distances) 
+					{
+						// ajouter un hystéresis
+						var testpresence = statistics.median(self.distances);
+						if(testpresence<=self.config.cstdist)
+						{
+							console.log("on !");
 						}
-                    }
-                    if(testpresence>self.config.cstdist)
-                    {
-                        presence=false;
-                        console.log(presence);
-                        self.config.delay=500;//5s
-                        if(firstoff==false)
-                        {
-                            console.log('ecran off');
-                            //exec("/opt/vc/bin/tvservice -o", null);
-							firstoff=true;
-                            firston=false;
-							
-                        }
-                   }
-           }
-           self.distances = [];      
-       }//fin du if
-        setTimeout(function () {
+						if(testpresence>self.config.cstdist)
+						{
+							console.log("off !");
+					    }
+					}
+			self.distances = [];      
+			}//fin du if
+        
+		
+		
+		
+		setTimeout(function () {
 	    self.distances.push(self.sensor());
-	    //console.log(self.config.rate+'ffffffffffffff '+ self.distances.length);
 	    if(self.distances.length>self.config.rate)//secu
 	    {
 	       console.log('overflow');
@@ -139,23 +102,7 @@ module.exports = NodeHelper.create({
 ///////////////////////////////////////////////
  } else if (notification === 'HARD') {
  
-	//changer et mettre des impulsions + un seul passage
-	if(payload == false )
-	{
-		hard=payload;
-		console.log('hard off node helper');
-	    setTimeout(function(){ self.relay.writeSync(0);}, 1000);
-		this.relay.writeSync(1); 
-		//self.sendSocketNotification("REARM");
-	}
-	if(payload == true )
-	{  
-		hard=payload;
-		console.log('hard on node helper');
-		setTimeout(function(){ self.relay.writeSync(0);}, 1000);
-		this.relay.writeSync(1); 
-		//self.sendSocketNotification("REARM");
-	}
+
 	 
  }
 },
